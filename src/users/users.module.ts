889f10f05cb810095f5031233635be1033deac98
Musalu Wambua <musaluwambua@gmail.com>
2022-07-15 16:17:47 +0300

    updated user module to include admin controller and service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,19 @@
-import { Properties, Users } from '@/models';
-import { PropertiesSchema } from '@/models/properties.models';
-import { UserSchema } from '@/models/users.models';
+import { Properties, PropertiesSchema, Users, UserSchema } from '@/models';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
 import { UserController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Users.name, schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: Properties.name, schema: PropertiesSchema }]),
+    MongooseModule.forFeature([
+      { name: Properties.name, schema: PropertiesSchema },
+    ]),
   ],
-  controllers: [UserController],
-  providers: [UsersService],
+  controllers: [UserController, AdminController],
+  providers: [UsersService, AdminService],
 })
 export class UserModule {}
